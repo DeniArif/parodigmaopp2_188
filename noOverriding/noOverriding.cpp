16190e5f19d1077fc3a117810fced0fe5b4ed296
DeniArif <deniarifwibowo82@gmail.com>
2023-05-29 19:56:39 +0700

    comit

---
+++ noOverriding/noOverriding.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class baseClass {
+public:
+	virtual void perkenalan() final {
+		cout << "Hallo saya Function dari class";
+	}
+};
+
+class 
+
+int main()
+
+
